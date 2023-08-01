3d6b07d22da800c27e74cf61aaa9d0c7837fe713
chmodke <13772804655@163.com>
2023-08-02 00:05:41 +0800

    调试脚本

---
+++ debug.sh
@@ -0,0 +1,5 @@
+export GO111MODULE=on
+export CGO_ENABLED=0
+export GOOS=linux
+export GOARCH=amd64
+go build -o remote *.go
---
+++ ugit.sh
@@ -0,0 +1,8 @@
+git config --local --unset-all user.name
+git config --local --add user.name chmodke
+
+git config --local --unset-all user.email
+git config --local --add user.email 13772804655@163.com
+
+git config --local --list
+
